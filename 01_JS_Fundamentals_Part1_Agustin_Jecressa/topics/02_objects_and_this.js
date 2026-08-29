e156269753bcebfa9cae3c8b8949939dad959d95
J-Llama <jecressagustin321@gmail.com>
2026-08-29 21:28:58 +0800

    01_JS_Fundamentals_Part1 > module 2 update -- complete

--- 01_JS_Fundamentals_Part1_Agustin_Jecressa/topics/02_objects_and_this.js
+++ 01_JS_Fundamentals_Part1_Agustin_Jecressa/topics/02_objects_and_this.js
@@ -25,6 +25,7 @@ export function GCashAccount(accountName, initialBalance = 0) {
 
 export function getBarangayName(resident) {
   // TODO: Use optional chaining resident?.address?.barangay?.name
+  return resident?.address?.barangay?.name || 'Unregistered Barangay';
 }
 
 export function runObjectsTests() {
