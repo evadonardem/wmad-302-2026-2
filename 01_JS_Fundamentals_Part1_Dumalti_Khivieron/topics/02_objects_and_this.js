dd93ac22a838975025b77d329d54d21246bc196f
Dumalti_K <2025-1-0901.dumalti@kcp.edu.ph>
2026-08-31 20:30:14 +0800

    01_JS_Fundamentals_Part1 --Module2 Task1,2 Complete

--- 01_JS_Fundamentals_Part1_Dumalti_Khivieron/topics/02_objects_and_this.js
+++ 01_JS_Fundamentals_Part1_Dumalti_Khivieron/topics/02_objects_and_this.js
@@ -5,10 +5,25 @@ export function GCashAccount(accountName, initialBalance = 0) {
   this.balance = initialBalance;
 
   // TODO: Implement cashIn(amount), sendMoney(amount, recipient), and getBalance()
-}
-
+    this.cashIn = function(amount){
+    this.balance += amount;
+    return this;
+    };
+    this.sendMoney = function(amount, recipient){
+      if(this.balance < amount + 15){
+        throw new Error('Insufficient GCash Balance');
+      }
+      this.balance -= amount + 15;
+      return this;
+    };
+    this.getBalance = function(){
+      return `₱${this.balance.toFixed(2)}`;
+    };
+  }
 export function getBarangayName(resident) {
   // TODO: Use optional chaining resident?.address?.barangay?.name
+    return resident?.address?.barangay?.name??`Unregistered Barangay`;
+
 }
 
 export function runObjectsTests() {
