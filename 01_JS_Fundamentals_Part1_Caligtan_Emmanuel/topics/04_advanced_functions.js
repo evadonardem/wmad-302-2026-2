091aef3161a756b5311031e2f0d472f72b3c6fc6
caligtan <2025-1-0410.caligtan.kcp.edu.ph>
2026-09-13 17:18:21 +0800

    01_JS_Fundamentals_Part_1 -- Cache evaluation results in a local object closure

--- 01_JS_Fundamentals_Part1_Caligtan_Emmanuel/topics/04_advanced_functions.js
+++ 01_JS_Fundamentals_Part1_Caligtan_Emmanuel/topics/04_advanced_functions.js
@@ -2,6 +2,17 @@ import console from 'node:console';
 
 export function memoize(fn) {
   // TODO: Cache evaluation results in a local object closure
+  const cache = {};
+
+  return function (...args) {
+    const key = JSON.stringify(args);
+    if (key in cache) {
+      return cache[key];
+    }
+    const result = fn.apply(this, args);
+    cache[key] = result;
+    return result;
+  };
 }
 
 export function createJeepneyFareCalculator(baseFare = 13, discountRate = 0.20) {
